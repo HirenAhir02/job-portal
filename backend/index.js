e6048c15bf2570dc54a230826a76fc4f369f9553
hirenahir02 <hirenahir509@gmailc.com>
2025-07-27 12:16:49 +0530

    cors setup done

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://job-portal-ui-psi.vercel.app/',
     credentials: true
 }
 app.use(cors(corsOptions));
